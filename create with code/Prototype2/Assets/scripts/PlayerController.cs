bdb861a134aef27a296e83a7ca5694999117583a
colin bratton <colinbratton3@gmail.com>
2022-10-11 10:23:49 -0700

    i tried to make the steak move but it didnt work

--- create with code/Prototype2/Assets/scripts/PlayerController.cs
+++ create with code/Prototype2/Assets/scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
 public float horizontalInput;
 public float speed = 10.0f;
 public float xRange = 10.0f;
+   
+public GameObject projectilePrefab;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +24,24 @@ public float xRange = 10.0f;
         {
             transform.position = new Vector3(-xRange, transform.position.y,transform.position.z);
         }
-         if (transform.position.x > xRange)
-       {
+        
+        if (transform.position.x > xRange)
+        {
             transform.position = new Vector3(xRange, transform.position.y,transform.position.z);
         }
-       horizontalInput = Input.GetAxis("Horizontal");
-       transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+      
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
+
+
+
+
 
 }
