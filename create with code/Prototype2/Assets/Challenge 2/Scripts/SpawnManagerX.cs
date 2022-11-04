4a8bb3aa4c7673891550ef1db04201390b248807
colin bratton <colinbratton3@gmail.com>
2022-11-04 10:39:14 -0700

    made dogs spawn when space is clicked

--- create with code/Prototype2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ create with code/Prototype2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,23 +10,25 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 0;
     private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
-
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay);
+        Invoke("SpawnRandomBall", startDelay);
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        startDelay = Random.Range(3, 5);
+
+        Invoke("SpawnRandomBall", startDelay);
+        
         // Generate random ball index and random spawn position
        int ballIndex = Random.Range(0, ballPrefabs.Length);
-        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), 0, spawnPosY, 0);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[animalIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 
 }
