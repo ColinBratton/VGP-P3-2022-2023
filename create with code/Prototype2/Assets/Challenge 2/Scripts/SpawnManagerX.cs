99bb01135938e106ec22fe7db00fab578dcbd911
colin bratton <colinbratton3@gmail.com>
2022-11-14 10:20:18 -0800

    made it more accurate

--- create with code/Prototype2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ create with code/Prototype2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -9,6 +9,7 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXLeft = -20;
     private float spawnLimitXRight = 0;
     private float spawnPosY = 30;
+    //public int Index = (0, 2);
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +25,11 @@ public class SpawnManagerX : MonoBehaviour
         Invoke("SpawnRandomBall", startDelay);
         
         // Generate random ball index and random spawn position
-       int ballIndex = Random.Range(0, ballPrefabs.Length);
+       int index = Random.Range(0, ballPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        Instantiate(ballPrefabs[index], spawnPos, ballPrefabs[index].transform.rotation);
     }
 
 }
