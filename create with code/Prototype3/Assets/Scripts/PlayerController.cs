49e79f0fee724e4f5c2549d4eae272e50176acfa
colin bratton <colinbratton3@gmail.com>
2022-11-17 10:41:15 -0800

    made player jump

--- create with code/Prototype3/Assets/Scripts/PlayerController.cs
+++ create with code/Prototype3/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 { 
+   private Rigidbody playerRb;
+   public float JumpForce = 10; 
+   public float gravityModifier;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) 
+    {
+        playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
     }
+    
+    }
+
 }
