2e157ba9ba933710276a4c985f7d3a3c4011120a
colin bratton <colinbratton3@gmail.com>
2023-01-04 10:44:29 -0800

    built the wall

--- create with code/PersonProject/Assets/Scripts/PlayerController.cs
+++ create with code/PersonProject/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     private float speed = 10.0f;
     private Rigidbody playerRb;
+    private float zBound = 6;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +17,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = +Input.GetAxis("Horizontal");
+        float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        playerRb.AddForce(Vertical3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        if(transform.position.z > zBound)
+        {
+           transform.position = new Vector3(transform.position.x, transform.position.y, -zBound); 
+        }
     }
 }
